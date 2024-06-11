3e740392d214cd52183775fc96400ab563ff907c
VIA2NEY <kouameangeV11@gmail.com>
2024-06-11 22:54:05 +0000

    My Run

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,17 @@ import 'package:plant_app/app_behavior.dart';
 import 'package:plant_app/ui/onboarding_screen.dart';
 import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(
-      DevicePreview(
-        enabled: true,
-        tools: const [
-          ...DevicePreview.defaultTools,
-        ],
-        builder: (context) => MyApp(), // Wrap your app
-      ),
-    );
+// void main() => runApp(
+//       DevicePreview(
+//         enabled: true,
+//         tools: const [
+//           ...DevicePreview.defaultTools,
+//         ],
+//         builder: (context) => MyApp(), // Wrap your app
+//       ),
+//     );
 
-// void main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
